1cc5fcb71a5cc1544edd03811e86b7c2ad3af667
mharika0402 <139908063+mharika0402@users.noreply.github.com>
2026-06-28 14:58:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-mkk
+PORT=5000
+MONGODB_URI=mongodb+srv://interviewai:Harika123@cluster0.6d4tcb8.mongodb.net/interviewai?retryWrites=true&w=majority&appName=Cluster0
+JWT_SECRET=my_super_secret_jwt_key_change_this_later
+
